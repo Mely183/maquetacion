1879e1d31c3aac190494870bc2be2e566bda105b
zambrix20 <companero20112011@hotmail.com>
2023-09-21 20:32:12 -0600

    view_post-getInfoById

--- view_post/post.js
+++ view_post/post.js
@@ -1,31 +1,43 @@
-// const URL_API ='https://devs-imparables-default-rtdb.firebaseio.com';
+const URL_API = 'https://devs-imparables-default-rtdb.firebaseio.com/';
 
-// const URL_API2 = 'https://jsonplaceholder.typicode.com/users';
-
-// alert('que ondaa')
-
-const search =window.location.search
+const search = window.location.search
+//console.log(search);
 const url = new URLSearchParams(search)
-const idpost = url.get('id');
+const ID_POST = url.get('id');
 
 const postTitle = document.querySelector('#post-title');
+const postDescription = document.querySelector('#post-description');
 
-const postDescription =document.querySelector('#post-description');
+const buttonEdit = document.querySelector('#buttonEdit')
 
-const getInfoById = async () =>{
-    try {
-        const url = URL_API2 + '?=1' + '.json'
-        const data = await fetch(url);
+buttonEdit.dataset.post = ID_POST
 
-    const parsed = await data.json();
+// Boton editar
+buttonEdit.addEventListener('click', (event) => {
+    console.log(window);
+    const elementToEdit = event.target.dataset.post
+
+    // para cambiar de vista
+    //window.location.href = 'http://127.0.0.1:5500/17-Crud/update/'
+    //window.location.href = 'http://127.0.0.1:5500/17-Crud/update/?id=' + elementToEdit + '&test=hola';
+    window.location.href = 'http://127.0.0.1:5500/edit_post/?id=' + elementToEdit
+    //window.location.pathname = '17-CRUD/update/'
+})
+
+// Traer informacion de ese hash
+const getInfoById = async () => {
+    const url = URL_API + ID_POST + '.json'
+    console.log(url);
+    const info = await fetch(url)
+    console.log(info);
+
+    const parsed = await info.json()
     console.log(parsed);
-    postTitle.value = parsed.name
-    postDescription.value = parsed.website 
-    }
-    catch(error){
-        console.error(error);
-    }
+
+    // Para que se muestren en los inputs
+    postTitle.textContent = parsed.titulo
+    postDescription.textContent = parsed.desciption
 }
 
+// console.log(getInfoById());
 getInfoById()
-
