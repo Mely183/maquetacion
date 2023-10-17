39f51444e643dc3f83ce85b339e1d39814a37895
ceciliaramoscavazos <aceciliaramos@gmail.com>
2023-10-17 17:19:46 -0600

    post.js port changed

--- view_post/post.js
+++ view_post/post.js
@@ -1,4 +1,5 @@
 const URL_API = 'https://devs-imparables-default-rtdb.firebaseio.com/';
+const port = 3000
 
 const search = window.location.search
 //console.log(search);
@@ -11,72 +12,18 @@ const postDescription = document.querySelector('#post-description');
 
 const buttonEdit = document.querySelector('#buttonEdit')
 
-//buttonEdit.dataset.post = ID_POST
-
-//const sectionContainer = document.querySelector('#section-Container');
-
-// const renderPost = (cardInfo) => {
-//     const cardDiv = document.createElement('div');
-//     const coverImg = document.createElement('img');
-//     const sectionFisrt = document.createElement('section');
-//     const divButton = document.createElement('div');
-//     const buttonEdit = document.createElement('button');
-//     const divCardBody = document.createElement('div');
-//     const title = document.createElement('h1');
-//     const sectionTags = document.createElement('section');
-//     const ulTags = document.createElement('ul');
-//     const liTags = document.createElement('li');
-//     const description = document.createElement('p');
-
-//     cardDiv.className = 'card';
-//     coverImg.className = 'card-img-top';
-//     coverImg.src = '/src/mago.webp'
-//     sectionFisrt.className = 'poster-information__container d-flex mb-3 ps-4 pt-3 gap-3';
-//     divButton.className = 'button-edit ms-auto pe-4';
-//     buttonEdit.className = 'button-edit-color';
-//     buttonEdit.id = 'buttonEdit';
-//     // buttonEdit.dataset.post = ID_POST;
-//     divCardBody.className = 'card-body';
-//     title.className = 'card-title ps-4';
-//     title.id = 'post-title';
-//     sectionTags.className = 'hashtag__container';
-//     sectionTags.id = 'post-hashtag';
-//     ulTags.className = 'd-flex flex-wrap list-unstyled';
-//     liTags.className = 'ps-4 me-2';
-//     description.className = 'card-text ps-4';
-//     description.id = 'post-description';
-
-//     ulTags.appendChild(liTags);
-//     sectionTags.appendChild(ulTags);
-//     divCardBody.appendChild(title);
-//     divCardBody.appendChild(sectionTags);
-//     divCardBody.appendChild(description);
-//     divButton.appendChild(buttonEdit);
-//     sectionFisrt.appendChild(divButton);
-//     sectionFisrt.appendChild(cardDiv);
-//     cardDiv.appendChild(coverImg);
-//     cardDiv.appendChild(sectionFisrt);
-//     cardDiv.appendChild(divCardBody);
-
-//     sectionContainer.appendChild(cardDiv);
-// }
-
 // Boton editar
 buttonEdit.addEventListener('click', (event) => {
     console.log(window);
     const elementToEdit = event.target.dataset.post
 
-    // para cambiar de vista
-    //window.location.href = 'http://127.0.0.1:5500/17-Crud/update/'
-    //window.location.href = 'http://127.0.0.1:5500/17-Crud/update/?id=' + elementToEdit + '&test=hola';
     window.location.href = '../edit_post/edit_post.html?id=' + ID_POST
-    //window.location.pathname = '17-CRUD/update/'
 })
 
 // Traer informacion de ese hash
 const getInfoById = async () => {
     const url = URL_API + ID_POST + '.json'
-    //const url = URL_API + '-NeugAeo2dr-wmPly7s0' + '.json'
+
     console.log(url);
     const info = await fetch(url)
     console.log(info);
@@ -90,5 +37,4 @@ const getInfoById = async () => {
     postDescription.textContent = parsed.description
 }
 
-// console.log(getInfoById());
 getInfoById()
