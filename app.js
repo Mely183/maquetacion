434e5dae8bc5cf7e3bea1f14c4e89e539b23b1d8
ceciliaramoscavazos <aceciliaramos@gmail.com>
2023-10-16 22:07:23 -0600

    db connected

---
+++ app.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+/** Directories */
+// const routes = require('./routes/index')
+// const docs = require('./doc/index')
+const swaggerJSDoc = require('swagger-jsdoc')
+const swaggerUI = require('swagger-ui-express')
+// const swaggerSpec = swaggerJSDoc(docs)
+const db = require('./lib/db')
+// const errorHandler = require('./middlewares/errorHandler')
+
+/** Middlewares */
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+db.connect()
+
+// app.use('/api-doc', swaggerUI.serve, swaggerUI.setup(swaggerSpec))
+
+/** Routes */
+// app.use(routes)
+// app.use(errorHandler.errorHandler)
+
+
+/** Server - Bucle que va a estar escuchando constantemente */
+app.listen(port, () => {
+  console.log(`Server app listening on port ${port}`)
+})
+
