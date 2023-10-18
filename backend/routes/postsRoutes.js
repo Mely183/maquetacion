192c70172eca5c4881213da43b1a1d6bdee630c7
zambrix20 <companero20112011@hotmail.com>
2023-10-17 21:29:36 -0600

    backend - posts methods are working and almost completed

--- backend/routes/postsRoutes.js
+++ backend/routes/postsRoutes.js
@@ -3,11 +3,18 @@ const express = require('express')
 const router = express.Router()
 
 const postController = require('../controllers/postsControllers')
+//const auth = require('../middlewares/auth')
 
 router.get('/posts', postController.getAll)
 
 router.get('/posts/:id', postController.getById)
 
 router.post('/posts', postController.post)
+
+//router.delete('/posts/:id', auth.authToken, postController.delete)
+router.delete('/posts/:id', postController.delete)
+
+//router.put('/posts/:id', auth.authToken  ,postController.put)
+router.put('/posts/:id', postController.put)
 
 module.exports = router
